fd465178d763b756b88f6d4be7046aa3434fe7b0
Filipe Fernandes <fmmfhd@gmail.com>
2019-11-29 16:25:46 +0000

    Log microservice

--- Log/app.py
+++ Log/app.py
@@ -3,6 +3,7 @@ from flask import render_template
 from flask import request
 from flask import jsonify
 import requests
+import json
 
 import pickle
 
@@ -36,8 +37,12 @@ class Logs:
 
     def showNLogs(self,N):
         log = []
-        for i in range(N):
-            log.append(self.db[-1-i])
+        print(N)
+        if len(self.db) > 0:
+            if N > len(self.db):
+                return self.db
+            for i in range(N):
+                log.append(self.db[-1-i])
         return log
 
 db = Logs("MyLib")
@@ -45,19 +50,33 @@ db = Logs("MyLib")
 
 @app.route('/logs')
 def showLogs():
-    db.showLogs
-    pass
+    try:
+        logs = db.showLogs()
+        resp = jsonify(logs)
+        resp.status_code = 200
+    except:
+        resp = jsonify("Unuccess")
+        resp.status_code = 400 
+    return resp
     
 
 @app.route('/logs/<int:number>')
 def showNLogs(number):
-    logs = db.showNLogs(number)
-    pass
+    try:
+        logs = db.showNLogs(number)
+        if logs == []:
+            resp = jsonify("Não há Logs")
+        else:
+            resp = jsonify(logs)
+        resp.status_code = 200
+    except:
+        resp = jsonify("Unuccess")
+        resp.status_code = 400 
+    return resp
     
-
 @app.route('/logs',methods=['POST','PUT'])
 def AddLog():
-    if request.is_json():
+    if request.is_json:
         db.add(request.json['data'])
         resp = jsonify("Success")
         resp.status_code = 200
@@ -65,33 +84,9 @@ def AddLog():
         resp = jsonify("Unsuccess")
         resp.status_code = 400
     return resp
-        
-    
-
-def format_message(old,type,date):
-    new = {}
-    new["name"] = "Logs"
-    new["info"] = {}
-    for key in old:
-        if date != None and date != key["day"]:
-            continue
-        new["info"][key["day"]]={}
-        for element in key['meal']:
-            if type != None and type.capitalize() != element['type']:
-                continue
-            new["info"][key["day"]][element['type']] = []
-            for element1 in element['info']:
-                new["info"][key['day']][element['type']].append(element1['name']) # Também posso pôr em lista
-    return new
-
-def format_date(numero):
-    ano = numero%10000
-    mes = int((numero%1000000 - numero%10000)/10000)
-    dia = int((numero-numero%1000000)/1000000)
-    return "%d/%d/%d"%(dia,mes,ano)
 
 if __name__ == '__main__':
     # app.run()
     # app.run(debug=True)
-    cfg = config.Canteen()
+    cfg = config.Log()
     app.run(debug=True, host=cfg.host, port=cfg.port)
