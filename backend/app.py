f0b600571913cb7e903090ce2abf965e0533a8d0
Filipe Fernandes <fmmfhd@gmail.com>
2019-11-30 15:58:58 +0000

    Implementei o show logs

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, flash
 from flask import render_template
-from flask import request, session, abort
+from flask import request, session, abort, redirect, url_for
 from flask import jsonify
 import requests
 import os
@@ -24,7 +24,15 @@ def log():
     log['info'] = ('Server %s %s')%(request.method, request.url)
     data['data'] = log
     r = requests.post(uri, json=data)
-    print(r.text)
+    print("Register a log was a " + r.text)
+
+@app.before_request
+def checkLogging():
+    path_split = request.path.split('/')
+    if path_split[1] == 'admin' and request.path != '/admin/login':
+        if not session.get('logged_in'):
+            return render_template("login.html")
+        
 
 ##################HTML/API###############################
 @app.route('/API/<path:path>')
@@ -34,18 +42,36 @@ def get_dir(path):
 ##################ADMIN/LOG################################
 @app.route('/admin')
 def admin_main():
-    if not session.get('logged_in'):
-        return render_template('login.html')
-    else:
-        return render_template('mainadmin.html')
+    return render_template('mainadmin.html')
 
-@app.route('/login', methods=['POST'])
+@app.route('/admin/login', methods=['POST'])
 def do_admin_login():
     if request.form['password'] == 'admin' and request.form['username'] == 'admin':
         session['logged_in'] = True
     else:
         flash('wrong password!')
-    return admin_main()
+    return redirect(url_for('admin_main'))
+
+@app.route("/admin/showServices")
+def showservices():
+    return "showservices"
+
+@app.route("/admin/showLogs", methods=['POST','GET'])
+def showLogs():
+    try:
+        url = ("%s/%s")%(uri,str(request.form['numberOfLogs']))
+    except:
+        url = uri
+    print(url)
+    r = requests.get(url)
+    if r.status_code != 200:
+        return redirect(url_for('admin_main'))
+    return render_template("logsTemplate.html", logs=r.json())
+
+@app.route("/admin/queryLogs")
+def queryLogs():
+    return render_template("logs_query.html")
+
 
 
 if __name__ == '__main__':
