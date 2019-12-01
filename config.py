b9f70d99a9013c377717814ecabff9633dc69f27
Filipe Fernandes <fmmfhd@gmail.com>
2019-12-01 15:13:19 +0000

    Change to add a method to return a dictionary

--- config.py
+++ config.py
@@ -22,3 +22,11 @@ class Log:
    def  __init__(self):
        self.host = '127.0.0.1'
        self.port = 5002
+    
+def dictMicroservices():
+    dicio = {}
+    dicio['Canteen'] = "http://%s:%d"%(Canteen().host,Canteen().port)
+    dicio['Log'] = "http://%s:%d"%(Log().host,Log().port)
+    dicio['Services'] = "http://%s:%d"%(Services().host,Services().port)
+    dicio['Rooms'] = "http://%s:%d"%(Rooms().host,Rooms().port)
+    return dicio
