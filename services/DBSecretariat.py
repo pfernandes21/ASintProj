a646c20c2b18b902d02b6f9514b9a245397db054
Filipe Fernandes <fmmfhd@gmail.com>
2019-12-13 11:44:20 +0000

    Mudei service para secretariat

--- services/DBSecretariat.py
+++ services/DBSecretariat.py
@@ -1,7 +1,10 @@
 import uuid
 import pickle
 
-class Service:
+class Secretariat:
+    """
+    Class that represent one Secretariat
+    """
     def __init__(self,Location,Name,Description,OpenTime):
         self.Location = Location
         self.Name = Name
@@ -9,51 +12,54 @@ class Service:
         self.OpenTime = OpenTime
         self.id = str(uuid.uuid1())
 
-class DBService:
+class DBSecretariat:
+    """
+    DataBase that saves all the Secretariats
+    """
     def __init__(self, name):
         self.name = name
         try:
-                f = open('serviceDB'+self.name, 'rb')
+                f = open('secretariatDB'+self.name, 'rb')
                 self.db = pickle.load(f)
                 f.close()
         except IOError:
                 self.db = {}
 
-    def addService(self,location,name,description,opening):
-        service = Service(location,name,description,opening)
-        self.db[service.id] = service
-        f = open('serviceDB'+self.name, 'wb')
+    def addSecretariat(self,location,name,description,opening):
+        secretariat = Secretariat(location,name,description,opening)
+        self.db[secretariat.id] = secretariat
+        f = open('secretariatDB'+self.name, 'wb')
         pickle.dump(self.db, f)
         f.close()
 
-    def rmService(self,id):
+    def rmSecretariat(self,id):
         del self.db[id]
-        f = open('serviceDB'+self.name, 'wb')
+        f = open('secretariatDB'+self.name, 'wb')
         pickle.dump(self.db, f)
         f.close()
 
-    def changeService(self,id,key,value):
+    def changeSecretariat(self,id,key,value):
         try:
-            service = self.db[id]
+            secretariat = self.db[id]
         except:
             return "Wrong ID"
         try:
             if key.lower() == "location": 
-                service.Location = value
+                secretariat.Location = value
             elif key.lower() == "name": 
-                service.Name = value
+                secretariat.Name = value
             elif key.lower() == "description": 
-                service.Description = value
+                secretariat.Description = value
             elif key.lower() == "opentime": 
-                service.OpenTime = value
+                secretariat.OpenTime = value
         except:
             return "Wrong Json"
-        f = open('serviceDB'+self.name, 'wb')
+        f = open('secretariatDB'+self.name, 'wb')
         pickle.dump(self.db, f)
         f.close()
         return "Success"
 
-    def listAllServices(self):
+    def listAllSecretariats(self):
         services = {}
         flag = True
 
@@ -70,7 +76,10 @@ class DBService:
 
         return services
 
-    def showService(self,id):
+    def showSecretariat(self,id):
+        """
+        Return the secretariat that has an id equals to the argument id
+        """
         print(id)
         print(self.db[str(id)])
         return self.db[id]
